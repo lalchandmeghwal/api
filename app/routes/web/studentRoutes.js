8933aef05e5131f07cf803c43e1f4670bbaed484
LalChandMeghwal <lm9982333682@gmail.com>
2026-05-12 00:55:17 +0530

    something went change

--- app/routes/web/studentRoutes.js
+++ app/routes/web/studentRoutes.js
@@ -8,5 +8,11 @@ studnetRouters.get('/student-list', studentList);
 studnetRouters.post('/student-insert', studentInsert);
 studnetRouters.put('/studnet-update/:id', studnetUpdate);
 studnetRouters.delete('/studnet-delete/:id', studnetDelete);
+studnetRouters.get('/', (req, res) => {
+    res.send({
+        status: 1,
+        mgs:'Server is runing '
+    })
+})
 
 export default studnetRouters;
